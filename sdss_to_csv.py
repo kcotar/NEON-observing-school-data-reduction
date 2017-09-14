f93b5f6e298f8ed14840bc5cdfc728bd698481b9
Klemen Cotar <cotar.klemen@gmail.com>
2017-09-14 16:29:25 +0100

    SDSS spectra to csv

---
+++ sdss_to_csv.py
@@ -0,0 +1,16 @@
+from astropy.io import fits
+
+fits_file = 'J032224.fits'
+dat_file = fits_file[:-4]+'dat'
+
+hud_fits = fits.open(fits_file)
+
+spectra = hud_fits[1].data['flux']
+wvl = 10.0**hud_fits[1].data['loglam']
+
+dat_t = open(dat_file, 'w')
+for i in range(len(spectra)):
+  dat_t.write(str(spectra[i])+' '+str(wvl[i])+'\n')
+
+dat_t.close()
+hud_fits.close()
